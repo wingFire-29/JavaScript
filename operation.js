b49b5c5777576d6fb8b16288e07561e7743a4c87
Parag <paragshrivastavadatia@gmail.com>
2026-05-29 22:52:34 +0530

    data types summary

--- operation.js
+++ operation.js
@@ -1,18 +1,30 @@
 let value=3
 let negvalue=-value
-console.log(negvalue);
+// console.log(negvalue);
 
 // basic operations
-console.log(2+2);
-console.log(2-2);
-console.log(2*2);
-console.log(2/2);
-console.log(2%2);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
 
 // tricky operations
 str1="Hello"
 str2=" world"
-console.log(str1+str2);
+// console.log(str1+str2);
 
 
-console.log(10 + 1 + "2" );
+// console.log(10 + 1 + "2" );
+
+// avoid these types of comparision and operations
+// comparision
+console.log(null>0)
+console.log(null<=0)
+console.log(null==0)
+console.log(null>=0)
+
+// '==' equality operator and '<=','>=' comparison operator works differently in javascript
+
+// '===' triple eql to checks the datatype as well
+
