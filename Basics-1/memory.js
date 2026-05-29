e6a578263f5aca2767df58b977b3a8cea86f8604
Parag <paragshrivastavadatia@gmail.com>
2026-05-30 00:01:29 +0530

    strings and memory

---
+++ Basics-1/memory.js
@@ -0,0 +1,16 @@
+let myutubeName="Parag_Shrivastava"
+
+let anotherName="novaGaming"
+
+// console.log(myutubeName)
+// console.log(anotherName)
+
+let userOne={
+    name:"parag",
+    age:21
+}
+let userTwo=userOne
+
+userTwo.name="Nova"
+
+console.log(userOne.name)
---
+++ Basics-1/strings.js
@@ -0,0 +1,22 @@
+const name="Parag"
+const repo=25
+
+
+// dont use this method of string concatenation
+// console.log( "Master "+name+ repo + " repos")
+
+// dont use this method of string concatenation instead use backticks
+// console.log(`Hello my name is ${name} and my repo count is ${repo}`)
+// this method is called string interpulation
+
+
+const gameName = new String('ParagNova')
+// console.log(gameName[0])
+// console.log(gameName.length)
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf('N'))
+
+
+// Slicing
+const newName=gameName.substring(0,5)
+console.log(newName)
