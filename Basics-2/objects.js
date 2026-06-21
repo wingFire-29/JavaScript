52943c77455d3c0c05457598949477860f282d18
Parag <paragshrivastavadatia@gmail.com>
2026-06-21 14:35:12 +0530

    basics file upload

--- Basics-2/objects.js
+++ Basics-2/objects.js
@@ -1,3 +1,16 @@
 //singleton
 
 // object literal
+
+
+// destructuring an object
+const course={
+    course:"js",
+    price:"999",
+    courseInstructor:"Parag"
+}
+// console.log(course.courseInstructor)
+
+//de structure:
+const {courseInstructor: teacher}=course
+console.log(teacher)
